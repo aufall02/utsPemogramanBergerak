5d82074b960532bc8f6455a36c88e1bfb8120f2f
aufall02 <aufaluzumaki@gmail.com>
2023-05-19 17:30:43 +0700

    Edit Scroll bar

--- app/src/main/java/com/aufal/utspemogramanbergerak/MainActivity.java
+++ app/src/main/java/com/aufal/utspemogramanbergerak/MainActivity.java
@@ -2,17 +2,19 @@ package com.aufal.utspemogramanbergerak;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText nama, nim, textArea;
     Button btn_save, btn_gas;
-
+    ScrollView scrollView;
     Boolean isLogin = false;
 
 
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         return etText.getText().toString().trim().length() == 0;
     }
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,37 +33,52 @@ public class MainActivity extends AppCompatActivity {
         textArea = findViewById(R.id.re_textArea);
         btn_save   = findViewById(R.id.btn_simpan);
         btn_gas = findViewById(R.id.btn_gas);
+        scrollView = findViewById(R.id.scrollView);
+
 
 
         btn_save.setOnClickListener(view -> {
            if (!(isEmpty(nama) || isEmpty(nim))){
+
                textArea.setText(
                        "\"Selamat Datang "+  nama.getText().toString() +"\nApakah Kamu siap Untuk \nMenghancurkan Dunia !!!\""
                );
                nama.getText().clear();
                nim.getText().clear();
-
                isLogin = true;
+
+               scrollView.post(new Runnable() {
+                   @Override
+                   public void run() {
+                       scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+                   }
+               });
+
            }else {
                Toast.makeText(this,"masukkan data anda",Toast.LENGTH_SHORT).show();
            }
 
         });
 
+
        btn_gas.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View view) {
-              try {
                   if (!isLogin){
                       Toast.makeText(getBaseContext(), "Masukkan Nama Dan Nim Anda", Toast.LENGTH_LONG).show();
                   }else{
-                      System.out.println("test");
                       Intent intent = new Intent(MainActivity.this, Calculator.class);
                       startActivity(intent);
+                      textArea.getText().clear();
+                      isLogin = false;
+                      scrollView.post(new Runnable() {
+                          @Override
+                          public void run() {
+                              scrollView.fullScroll(ScrollView.FOCUS_UP);
+                          }
+                      });
                   }
-              }catch (Exception err){
-                  System.out.println(err);
-              }
+
            }
        });
 
